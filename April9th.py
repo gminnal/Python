7085e62aa6e7284b0b19ebef6f0dc8252175a900
gminnal <38123749+gminnal@users.noreply.github.com>
2018-04-09 16:34:42 +0530

    Add files via upload

---
+++ April9th.py
@@ -0,0 +1,32 @@
+#This is printing String
+
+print ("Hello World")
+
+#This is printing addition of numbers
+
+print(2+3)
+
+#Converting strings to int
+
+print (int("2")+int("3"))
+
+#Float addition
+
+print(2.5 + 2.7)
+
+
+print ("Don't")  #Printing '
+
+print ("She said Dont't  do tbat")
+
+print ('She said "Don\'t do that"')
+
+print ("Hello, " + "Python")
+
+print ("This costs " + str( 6 ) + " dollars") #Converting int to string
+
+print ("Arun : Minnal : Sireesha : Veda ".split(":")) #splits string with delimiiter into array.
+print ("Arun : Minnal : Sireesha : Veda ".split(":")[1]) #prints the first element in the array.
+
+print (5==5) #Checking for boolean value returns True
+print (5==6) #Checking for boolean value returns false
