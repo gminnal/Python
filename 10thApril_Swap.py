674981c4000797df21e50f4e92ac413d0d9ee557
gminnal <38123749+gminnal@users.noreply.github.com>
2018-04-10 17:08:57 +0530

    Add files via upload

---
+++ 10thApril_Swap.py
@@ -0,0 +1,27 @@
+list1= ["April", "May", "June"]
+print (list1[0])
+
+#swapping nos
+a=10
+b=20
+c=a
+a=b
+b=c
+print(a)
+print(b)
+
+
+#Dictonaries
+print ({"name" : "Arun", "age": 27, "Hobbies" : "Code"}['name'])
+print ({"name" : "Arun", "age": 27, "Hobbies" : "Code"}['age'])
+print ({"name" : "Arun", "age": 27, "Hobbies" : "Code"}['Hobbies'])
+
+#Variables
+a="Hello World"
+print(a)
+a=a.split(" ")[0]
+print(a)
+
+firstnumber=10
+secondnumber=20
+print (firstnumber+secondnumber)
---
+++ Functions_10thApril.py
@@ -0,0 +1,13 @@
+#Different builtin functions
+
+print (int("6"))
+print (float("9.2"))
+print (len("Hello"))
+list1=[2,3,4,5,6]
+print (len(list1))
+
+a=("Hello","Nick")
+print(len(a))
+
+list2=[12,4,2,3,6,9]
+print (sorted(list2))
